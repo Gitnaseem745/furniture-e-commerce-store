41e5200af1082f04a173d5fc1a1d76798a3ff54e
Naseem Ansari <mrnaseem745@gmail.com>
2024-11-23 23:00:33 +0530

    #code optimizations

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -3,16 +3,13 @@
 import Image from "next/image";
 
 const Partners: React.FC = () => {
-  return (
-    <div className="w-full max-h-fit px-20 py-12 flex gap-28 justify-center items-center bg-[#EEF4F5]">
-        <Image width={100} alt="" height={100} src={'images/brand-goldline.svg'} />
-        <Image width={120} alt="" height={100} src={'images/brand-magnolia.svg'} />
-        <Image width={100} alt="" height={100} src={'images/brand-boltshift.svg'} />
-        <Image width={100} alt="" height={100} src={'images/brand-contrast.svg'} />
-        <Image width={100} alt="" height={100} src={'images/brand-asgardia.svg'} />
-        <Image width={100} alt="" height={100} src={'images/brand-komplex.svg'} />
+    return (
+        <div className="w-full max-h-fit px-20 py-12 flex gap-28 justify-center items-center bg-[#EEF4F5]">
+        { images.map((img, i) => <Image width={100} alt={`Partner ${i+1}`} key={i} height={100} src={img} /> )}
     </div>
   )
 }
+
+const images = ["images/brand-goldline.svg", "images/brand-magnolia.svg", "images/brand-boltshift.svg", "images/brand-contrast.svg", "images/brand-asgardia.svg", "images/brand-komplex.svg"];
 
 export default Partners
