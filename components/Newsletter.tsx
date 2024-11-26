83c824e38be7598c8e6591da06a3943909af80df
Naseem Ansari <mrnaseem745@gmail.com>
2024-11-26 23:37:21 +0530

    #making newsletter responsive

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import Image from "next/image";
-import Button from "./Button";
 
 const Newsletter: React.FC = () => {
   return (
-    <div className="flex flex-row justify-between items-center px-32 text-white py-10 mb-[-49px] w-full bg-[#152420]">
+    <div className="flex flex-row justify-between items-center px-32 text-white py-10 mb-[-49px] w-full bg-[#152420] flex-wrap gap-10 max-md:justify-center">
         <div className="flex flex-row gap-10 justify-center items-center">
             <Image src={'/images/subscribe-section-image.webp'} width={60} height={100} alt="Newsletter Image"/>
             <div className="flex flex-col gap-1 justify-center items-start">
@@ -13,8 +12,8 @@ const Newsletter: React.FC = () => {
                 <p>Subscribe and grab 20% OFF!</p>
             </div>
         </div>
-        <div className="relative">
-            <input type="text" placeholder="Your email*" className="w-[600px] bg-transparent focus:outline-none border-[2px] border-[#264B4E] rounded-full p-5"/>
+        <div className="relative max-w-full">
+            <input type="text" placeholder="Your email*" className="w-[600px] max-md:w-[400px] bg-transparent focus:outline-none border-[2px] border-[#264B4E] rounded-full p-5"/>
             <button className="absolute top-[13%] right-2 bg-[#325c51] text-sm rounded-full hover:bg-[#354843] text-white w-[120px] h-[50px] justify-center items-center flex transition-all ease-in delay-50 ">Subscribe</button>
         </div>
     </div>
